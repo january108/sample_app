dfd6cd9881f652ac3e5f324a241307d2ab541fc8
january108 <january1122@gmail.com>
2017-05-07 22:45:33 +0900

    login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,8 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       # ユーザ情報のページへ
+      log_in user
+      redirect_to user
     else
       # エラーメッセージを表示
       flash.now[:danger] = "Invalid email/password combination"
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,16 @@
 module SessionsHelper
+    # 渡されたユーザでログインする
+    def log_in(user)
+        session[:user_id] = user.id
+    end
+    
+    # 現在ログイン中のユーザを返す（いる場合）
+    def current_user
+        @current_user ||= User.find_by(id: session[:user_id])
+    end
+    
+    # ユーザがログインしていればtrue、その他ならfalseを返す
+    def loged_in?
+        !current_user.nil?
+    end
 end
