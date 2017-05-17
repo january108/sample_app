c8977b1b3edd8101e1b68526065928be6c85e294
january108 <january1122@gmail.com>
2017-05-17 16:05:16 +0900

    integration test for index micropost

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -15,4 +15,17 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
     get user_url @user
     assert_template 'users/show'
   end
+  
+  test "profie display" do
+    get user_path(@user)
+    assert_template 'users/show'
+    assert_select 'title', full_title(@user.name)
+    assert_select 'h1', text: @user.name
+    assert_select 'h1>img.gravatar'
+    assert_match @user.microposts.count.to_s, response.body
+    assert_select 'div.pagination'
+    @user.microposts.paginate(page: 1).each do |micropost|
+      assert_match micropost.content, response.body
+    end
+  end
 end
