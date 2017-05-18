8fb105e95e256bc7b992dcd66cf65b77ee27212c
january108 <january1122@gmail.com>
2017-05-18 21:41:55 +0900

    minor modification

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -2,10 +2,8 @@ class MicropostsController < ApplicationController
   before_action :logged_in_user, only: [:create, :destroy]
   
   def create
-      p "micropost create"
   end
   
   def destroy
-      p "micropost destroy"
   end
 end
--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 class UsersProfileTest < ActionDispatch::IntegrationTest
+  include ApplicationHelper
+  
   def setup
     @user = users(:michael)
   end
