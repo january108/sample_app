93a84982e8cefd650ab232ed8698e213fbc69b4d
january108 <january1122@gmail.com>
2017-06-06 09:54:06 +0900

    email when signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,9 +21,12 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      flash[:success] = "Welcome to the Sample App (^^)/"
-      redirect_to @user
+      # log_in @user
+      # flash[:success] = "Welcome to the Sample App (^^)/"
+      # redirect_to @user
+      UserMailer.account_activation(@user).deliver_now
+      flash[:info] = "Please check your email to activate your accnout."
+      redirect_to root_url
       #debugger
     else
       render 'new'
